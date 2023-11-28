704c439545bcc1fb4bab4f57312194c55cedc64d
abk777-cell <81944011+abk777-cell@users.noreply.github.com>
2023-11-28 16:15:31 -0500

    Update train_hitnet.py

--- compubox/models/train/train_hitnet.py
+++ compubox/models/train/train_hitnet.py
@@ -51,7 +51,7 @@ if __name__ == '__main__':
     parser.add_argument('--epochs', nargs="?", default=100, type=int)
     parser.add_argument('--batch_size', nargs="?", default=5, type=int)
     parser.add_argument('--loss-every', nargs="?", default=1, type=int)
-    parser.add_argument('--output', '-o', nargs="?", default="./model.pth", type=str)
+    parser.add_argument('--output', '-o', nargs="?", default="Downloads/model.pth", type=str)
     args = parser.parse_args()
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
@@ -60,21 +60,36 @@ if __name__ == '__main__':
     X, y = gen_dataset(args.dataset_path, args.hit_keyword, args.miss_keyword)
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=args.train_split, random_state=42)
     expected_size = utils.get_max_img_size(X)
+    print(expected_size)
 
-    net = HitNet(img_size=expected_size)
+    net = HitNet()
     net.to(device)
     criterion = nn.BCEWithLogitsLoss()
+    # Define the transformation
+    transform = transforms.Compose([
+    transforms.Resize((647, 916)),
+    transforms.Grayscale(num_output_channels=3),  # Convert to RGB if your image is grayscale
+    transforms.ToTensor(),
+    ])
+
 
     running_loss = 0.0
 
     torch.cuda.empty_cache()
+
+    for i in tqdm(range(len(X_train))):
+        img = X_train[i]
+        img = utils.resize_with_padding(img, expected_size)
+        img = transforms.ToPILImage()(img)
+        img = transform(img)
+        img = img.unsqueeze(0)
+        X_train[i] = img
 
     for epoch in range(args.epochs):
         optimizer = optim.Adam(net.parameters(), lr=args.learning_rate)#, momentum=args.momentum)
         print(f"Running epoch {epoch + 1}/{args.epochs}")
         for i in tqdm(range(len(X_train))):
             img = X_train[i]
-            img = utils.resize_with_padding(img, expected_size)
             optimizer.zero_grad()
             output = net(img.to(device))
             t = torch.tensor(y_train[i]).reshape(output.shape).to(torch.float).to(device)
